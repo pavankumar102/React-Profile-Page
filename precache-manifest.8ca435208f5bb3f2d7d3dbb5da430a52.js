6958d0c8dd155602f25684f06db42f3632ac8d4a
G.pavan kumar <64958098+pavankumar102@users.noreply.github.com>
2020-07-07 10:37:24 +0530

    Updates

---
+++ precache-manifest.8ca435208f5bb3f2d7d3dbb5da430a52.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "af1d57e37d2813fecb58",
+    "url": "/React-Profile-Page/static/css/main.1bb044a2.chunk.css"
+  },
+  {
+    "revision": "af1d57e37d2813fecb58",
+    "url": "/React-Profile-Page/static/js/main.5640562a.chunk.js"
+  },
+  {
+    "revision": "597ba74bc42892c0436a",
+    "url": "/React-Profile-Page/static/js/runtime~main.fa35138c.js"
+  },
+  {
+    "revision": "252288afce0a3d0d487c",
+    "url": "/React-Profile-Page/static/js/2.bf3bfe8f.chunk.js"
+  },
+  {
+    "revision": "49116dca046614debdbaecd622e9dcdb",
+    "url": "/React-Profile-Page/static/media/pavankumar.49116dca.jpg"
+  },
+  {
+    "revision": "deb493608dae9e533ef6b6894095b797",
+    "url": "/React-Profile-Page/static/media/yt1.deb49360.png"
+  },
+  {
+    "revision": "eb0d38e2843d3a1025a6e30b84934c85",
+    "url": "/React-Profile-Page/static/media/biopicimg.eb0d38e2.jpg"
+  },
+  {
+    "revision": "c290a443b3f240732252a813d4a6cf65",
+    "url": "/React-Profile-Page/static/media/pythonimg.c290a443.png"
+  },
+  {
+    "revision": "184a7681dfc7ae09341edaafc979da8f",
+    "url": "/React-Profile-Page/static/media/javascriptimg2.184a7681.png"
+  },
+  {
+    "revision": "3a431a93c33adf4f325cfbd1547c8149",
+    "url": "/React-Profile-Page/static/media/html5logo.3a431a93.jpg"
+  },
+  {
+    "revision": "04cf122be0afe7542083d1a3967390ae",
+    "url": "/React-Profile-Page/static/media/csslogo.04cf122b.png"
+  },
+  {
+    "revision": "dd7784a9f6b6fd46106661b5c43afae1",
+    "url": "/React-Profile-Page/static/media/reactjs_logo.dd7784a9.png"
+  },
+  {
+    "revision": "1615105a709288658e77931a02110f23",
+    "url": "/React-Profile-Page/static/media/sql_logo.1615105a.jpg"
+  },
+  {
+    "revision": "8a9628381fa3cde3e5eb27e980ec3f80",
+    "url": "/React-Profile-Page/static/media/django.8a962838.png"
+  },
+  {
+    "revision": "6163d80dabb563a2f44bb03ad1ee3f41",
+    "url": "/React-Profile-Page/static/media/bootstrap.6163d80d.png"
+  },
+  {
+    "revision": "ed42215a6c98f3a6519c33d5d65c48fd",
+    "url": "/React-Profile-Page/static/media/web_page.ed42215a.png"
+  },
+  {
+    "revision": "cacf3bb032fd04c2adcf06ba1b261e46",
+    "url": "/React-Profile-Page/static/media/wishes.cacf3bb0.png"
+  },
+  {
+    "revision": "7083e454b70d153d8e9cdbd4aa29a9d7",
+    "url": "/React-Profile-Page/static/media/registration.7083e454.png"
+  },
+  {
+    "revision": "2e5c3718e826042725b145d4c1be6bd4",
+    "url": "/React-Profile-Page/static/media/javascript.2e5c3718.png"
+  },
+  {
+    "revision": "a2d6e1cb97282f21f1b7af90868ea468",
+    "url": "/React-Profile-Page/static/media/web_auto.a2d6e1cb.png"
+  },
+  {
+    "revision": "bb3af5906eff394f5418e5fdf4573c2f",
+    "url": "/React-Profile-Page/static/media/gmail_auto.bb3af590.png"
+  },
+  {
+    "revision": "5fe08886382840400ec22a0afd2a3fa4",
+    "url": "/React-Profile-Page/static/media/smarthome_UI.5fe08886.png"
+  },
+  {
+    "revision": "e38766a187961322358125fe8517de2d",
+    "url": "/React-Profile-Page/static/media/contactlogo.e38766a1.png"
+  },
+  {
+    "revision": "eb9139ef9b107a85095c1d8d411d71b1",
+    "url": "/React-Profile-Page/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -11,19 +11,13 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Profile-Page/precache-manifest.51daed57acbe110912811aca9b9f2100.js"
+  "/React-Profile-Page/precache-manifest.8ca435208f5bb3f2d7d3dbb5da430a52.js"
 );
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
-    self.skipWaiting();
-  }
-});
-
-workbox.core.clientsClaim();
+workbox.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -31,9 +25,10 @@ workbox.core.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Profile-Page/index.html"), {
+workbox.routing.registerNavigationRoute("/React-Profile-Page/index.html", {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
